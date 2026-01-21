74702d7d3a7f5ff953c58f4ae1e22a9bd1231567
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-01-21 14:13:36 +0100

    Agregamos el marcador con su fuente y color

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ pg.display.set_caption("PONG")
 #Definir tiempo de tasa de refreso dentro del bucle de fotogramas, fps = fotograma por segundo
 tasa_refresco = pg.time.Clock()
 
+#Agregar texto a marcadores
+#Asignación de fuente y tamaño de letra
+marcador1_font = pg.font.SysFont("verdana", 30)
+marcador2_font = pg.font.SysFont("verdana", 30)
+
+
+
+
 game_over = True
 
 #Todo lo que tenga que ver con la pantalla va dentro del while
@@ -32,7 +40,22 @@ while game_over:
     pelota.mover(800,600)
 
     pantalla_principal.fill( (35,78,198) )
-    pg.draw.line( pantalla_principal,(255, 255, 255),(400,0),(400,600), width = 10 )
+    """
+        #pg.draw.line( pantalla_principal,(255, 255, 255),(400,0),(400,600), width = 10 )
+
+    
+    raya = 15
+    espacio = 10
+    y = 0
+    #while y < 600:
+     #   pg.draw.line( pantalla_principal,(255, 255, 255),(x,y),(x, y + raya ), width = 10 )
+      #  y += raya + espacio
+      """
+    
+
+    x = 400
+    for i in range(0, 600, 20):
+        pg.draw.line( pantalla_principal,(255, 255, 255),(x,i),(x,i + 15), width = 10 )
 
     raqueta1.dibujar(pantalla_principal)
     raqueta2.dibujar(pantalla_principal)
@@ -42,6 +65,12 @@ while game_over:
     raqueta2.mover(pg.K_UP,pg.K_DOWN)
     print(pelota.contadorIzquierdo)
     print(pelota.contadorDerecho)
+    #Asiganción de color y texto
+    marcador1 = marcador1_font.render("10", True,(255, 255, 255))
+    marcador2 = marcador2_font.render("10", True,(255, 255, 255))
+    pantalla_principal.blit(marcador1,(310,50))
+    pantalla_principal.blit(marcador2, (450,50))
+    
     pg.display.flip()
 
 
